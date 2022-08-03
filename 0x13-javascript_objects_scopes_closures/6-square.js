b721b5c13ba82ec723f1dcf3ba91088c35891070
76khuwa <ntombimathibela97@gmail.com>
2022-08-03 11:19:15 +0200

    javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const Rectangle = require('./5-square');
 module.exports = class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
-      this.print(x);
+      this.print();
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
